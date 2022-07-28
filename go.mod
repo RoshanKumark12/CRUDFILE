c46596552d4191b5708c37b64ee721cb0f3abf70
Roshan <roshan.kumar5111999@gmail.com>
2022-07-28 14:07:38 +0530

    golang

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/Roshankumar/Demoproject
+
+go 1.18
+
+require (
+	github.com/99designs/gqlgen v0.17.12
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/jinzhu/gorm v1.9.16
+	//github.com/sirupsen/logrus v1.9.0
+	github.com/vektah/gqlparser/v2 v2.4.6
+)
+
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+	github.com/mitchellh/mapstructure v1.3.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	//	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,51 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Roshankumar/Demoproject/graph/generated"
+	"github.com/Roshankumar/Demoproject/graph/model"
+	"github.com/Roshankumar/Demoproject/test"
+)
+
+// CreateTodo is the resolver for the createTodo field.
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// UpsertStudent is the resolver for the upsertStudent field.
+func (r *mutationResolver) UpsertStudent(ctx context.Context, input *model.StudentInput) (*model.StudentResponse, error) {
+	//panic(fmt.Errorf("not implemented"))
+	result, err := test.TableInsert(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+	return result, err
+}
+
+// Todos is the resolver for the todos field.
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Datafetch is the resolver for the datafetch field.
+func (r *queryResolver) Datafetch(ctx context.Context, input *model.StudentData) ([]*model.StudentOutput, error) {
+	result, err := test.FetchData(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+	return result, err
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
